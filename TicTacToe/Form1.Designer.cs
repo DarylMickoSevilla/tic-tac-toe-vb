6b3b5e81f9a5bf9480bfb3b7f183996c7e944406
DarylMickoSevilla <mickosevilla@gmail.com>
2022-06-04 07:53:22 +0800

    So Restart doing it again

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L1 = new System.Windows.Forms.Button();
+            this.M1 = new System.Windows.Forms.Button();
+            this.R1 = new System.Windows.Forms.Button();
+            this.L2 = new System.Windows.Forms.Button();
+            this.M2 = new System.Windows.Forms.Button();
+            this.R2 = new System.Windows.Forms.Button();
+            this.L3 = new System.Windows.Forms.Button();
+            this.M3 = new System.Windows.Forms.Button();
+            this.R3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // L1
+            // 
+            this.L1.BackColor = System.Drawing.Color.Black;
+            this.L1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.L1.ForeColor = System.Drawing.Color.Red;
+            this.L1.Location = new System.Drawing.Point(146, 141);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(87, 70);
+            this.L1.TabIndex = 0;
+            this.L1.Text = "X";
+            this.L1.UseVisualStyleBackColor = false;
+            this.L1.Click += new System.EventHandler(this.onclick);
+            // 
+            // M1
+            // 
+            this.M1.BackColor = System.Drawing.Color.Black;
+            this.M1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.M1.ForeColor = System.Drawing.Color.Red;
+            this.M1.Location = new System.Drawing.Point(239, 141);
+            this.M1.Name = "M1";
+            this.M1.Size = new System.Drawing.Size(87, 70);
+            this.M1.TabIndex = 1;
+            this.M1.Text = "X";
+            this.M1.UseVisualStyleBackColor = false;
+            this.M1.Click += new System.EventHandler(this.onclick);
+            // 
+            // R1
+            // 
+            this.R1.BackColor = System.Drawing.Color.Black;
+            this.R1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R1.ForeColor = System.Drawing.Color.Red;
+            this.R1.Location = new System.Drawing.Point(332, 141);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(87, 70);
+            this.R1.TabIndex = 2;
+            this.R1.Text = "X";
+            this.R1.UseVisualStyleBackColor = false;
+            this.R1.Click += new System.EventHandler(this.onclick);
+            // 
+            // L2
+            // 
+            this.L2.BackColor = System.Drawing.Color.Black;
+            this.L2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.L2.ForeColor = System.Drawing.Color.Red;
+            this.L2.Location = new System.Drawing.Point(146, 217);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(87, 70);
+            this.L2.TabIndex = 3;
+            this.L2.Text = "X";
+            this.L2.UseVisualStyleBackColor = false;
+            this.L2.Click += new System.EventHandler(this.onclick);
+            // 
+            // M2
+            // 
+            this.M2.BackColor = System.Drawing.Color.Black;
+            this.M2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.M2.ForeColor = System.Drawing.Color.Red;
+            this.M2.Location = new System.Drawing.Point(239, 217);
+            this.M2.Name = "M2";
+            this.M2.Size = new System.Drawing.Size(87, 70);
+            this.M2.TabIndex = 4;
+            this.M2.Text = "X";
+            this.M2.UseVisualStyleBackColor = false;
+            this.M2.Click += new System.EventHandler(this.onclick);
+            // 
+            // R2
+            // 
+            this.R2.BackColor = System.Drawing.Color.Black;
+            this.R2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R2.ForeColor = System.Drawing.Color.Red;
+            this.R2.Location = new System.Drawing.Point(332, 217);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(87, 70);
+            this.R2.TabIndex = 5;
+            this.R2.Text = "X";
+            this.R2.UseVisualStyleBackColor = false;
+            this.R2.Click += new System.EventHandler(this.onclick);
+            // 
+            // L3
+            // 
+            this.L3.BackColor = System.Drawing.Color.Black;
+            this.L3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.L3.ForeColor = System.Drawing.Color.Red;
+            this.L3.Location = new System.Drawing.Point(146, 293);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(87, 70);
+            this.L3.TabIndex = 6;
+            this.L3.Text = "X";
+            this.L3.UseVisualStyleBackColor = false;
+            this.L3.Click += new System.EventHandler(this.onclick);
+            // 
+            // M3
+            // 
+            this.M3.BackColor = System.Drawing.Color.Black;
+            this.M3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.M3.ForeColor = System.Drawing.Color.Red;
+            this.M3.Location = new System.Drawing.Point(239, 293);
+            this.M3.Name = "M3";
+            this.M3.Size = new System.Drawing.Size(87, 70);
+            this.M3.TabIndex = 7;
+            this.M3.Text = "X";
+            this.M3.UseVisualStyleBackColor = false;
+            this.M3.Click += new System.EventHandler(this.onclick);
+            // 
+            // R3
+            // 
+            this.R3.BackColor = System.Drawing.Color.Black;
+            this.R3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R3.ForeColor = System.Drawing.Color.Red;
+            this.R3.Location = new System.Drawing.Point(332, 293);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(87, 70);
+            this.R3.TabIndex = 8;
+            this.R3.Text = "X";
+            this.R3.UseVisualStyleBackColor = false;
+            this.R3.Click += new System.EventHandler(this.onclick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(811, 460);
+            this.Controls.Add(this.R3);
+            this.Controls.Add(this.M3);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.M2);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.M1);
+            this.Controls.Add(this.L1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button L1;
+        private Button M1;
+        private Button R1;
+        private Button L2;
+        private Button M2;
+        private Button R2;
+        private Button L3;
+        private Button M3;
+        private Button R3;
+    }
+}
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,15 @@
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void onclick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
